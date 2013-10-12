e1f3e6f59acdc5220a0f3bd662009e9152237f32
Ruimin <zhangruimincn@gmail.com>
2013-10-12 14:20:33 +0200

    introduced GridLayout v7 support

--- src/com/lvtuxiongdi/packingcheck/PackingListFragment.java
+++ src/com/lvtuxiongdi/packingcheck/PackingListFragment.java
@@ -6,7 +6,7 @@ import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.GridLayout;
+import android.support.v7.widget.GridLayout;
 import android.widget.TextView;
 
 public final class PackingListFragment extends Fragment {
@@ -19,14 +19,14 @@ public final class PackingListFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View layout = inflater.inflate(R.layout.layout_list, container, false);
-        GridLayout viewById = (GridLayout)layout.findViewById(R.id.GridLayout1);
+        GridLayout grid = (GridLayout)layout.findViewById(R.id.GridLayout1);
         for(int i=0;i<12;i++){
             TextView text = new TextView(getActivity());
             text.setGravity(Gravity.CENTER);
             text.setText("a");
             text.setTextSize(20 * getResources().getDisplayMetrics().density);
             text.setPadding(20, 20, 20, 20);
-            viewById.addView(text);
+            grid.addView(text);
         }
 
 
