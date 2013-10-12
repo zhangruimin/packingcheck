602c6a25667fc27961d9e7b07bd0f0fee773b4aa
Ruimin <zhangruimincn@gmail.com>
2013-10-12 20:19:11 +0200

    use PagedDragDropGrid to show the grid

--- src/com/lvtuxiongdi/packingcheck/MainActivity.java
+++ src/com/lvtuxiongdi/packingcheck/MainActivity.java
@@ -1,34 +1,76 @@
 package com.lvtuxiongdi.packingcheck;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
-import android.support.v4.view.ViewPager;
-import com.viewpagerindicator.CirclePageIndicator;
-import com.viewpagerindicator.PageIndicator;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+import ca.laplanete.mobile.pageddragdropgrid.OnPageChangedListener;
+import ca.laplanete.mobile.pageddragdropgrid.PagedDragDropGrid;
 
-public class MainActivity extends FragmentActivity {
+public class MainActivity extends Activity implements View.OnClickListener {
 
-    MainFragmentAdapter mAdapter;
-    ViewPager mPager;
-    PageIndicator mIndicator;
+    private String CURRENT_PAGE_KEY = "CURRENT_PAGE_KEY";
 
-    /**
-     * Called when the activity is first created.
-     */
+    private PagedDragDropGrid gridview;
+
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+
+        setContentView(R.layout.layout_list);
+        gridview = (PagedDragDropGrid) findViewById(R.id.gridview);
+
+        ExamplePagedDragDropGridAdapter adapter = new ExamplePagedDragDropGridAdapter(this, gridview);
+
+        gridview.setAdapter(adapter);
+        gridview.setClickListener(this);
+
+        gridview.setBackgroundColor(Color.LTGRAY);
 
 
+        gridview.setOnPageChangedListener(new OnPageChangedListener() {
+            @Override
+            public void onPageChanged(PagedDragDropGrid sender, int newPageNumber) {
+                Toast.makeText(MainActivity.this, "Page changed to page " + newPageNumber, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
 
-        mAdapter = new MainFragmentAdapter(getSupportFragmentManager());
+    @Override
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        int savedPage = savedInstanceState.getInt(CURRENT_PAGE_KEY);
+        gridview.restoreCurrentPage(savedPage);
+    }
 
-        mPager = (ViewPager)findViewById(R.id.pager);
-        mPager.setAdapter(mAdapter);
 
-        mIndicator = (CirclePageIndicator)findViewById(R.id.indicator);
-        mIndicator.setViewPager(mPager);
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+
+        outState.putInt(CURRENT_PAGE_KEY, gridview.currentPage());
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        menu.add("Reset").setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                gridview.setAdapter(new ExamplePagedDragDropGridAdapter(MainActivity.this, gridview));
+                gridview.notifyDataSetChanged();
+
+                return true;
+            }
+        });
+
+        return true;
+    }
+
+    @Override
+    public void onClick(View v) {
+        Toast.makeText(this, "Clicked View", Toast.LENGTH_SHORT).show();
     }
 }
